9a79c00a3b22c613b6eb86dfbd71c999d22ed497
TaeYeong19 <mattam1129@gmail.com>
2022-11-17 23:28:05 +0900

    fetch_error

--- bg.js
+++ bg.js
@@ -1,11 +1,17 @@
-$(".front").remove();
+// $(".front").remove();
+// $(".c_title").remove();
 $("#orgData_vision").remove();
 $("#orgData_capa_plan").remove();
+// $(".part.advice.half").remove();
 $(".text_add").remove();
 $(".q_menu").remove();
 
-$(".col_md_6.img").after($(".section.my_file"));
-$(".col_md_6.img").remove();
+$(".col_md_6.img").append($(".section.my_file"));
+// $(".col_md_6.img").after($('<div class="uploadFile"></div>'));
+// $(".uploadFile").append($(".section.my_file"));
+$("img").eq(3).remove();
+$(".h_box").remove();
+// $(".date").remove();
 
 $(".col_md_12").eq(1).after($(".col_md_12"));
 
@@ -18,3 +24,8 @@ $(".col_md_12").eq(1).after($(".col_md_12"));
 
 $(".section.mooc").remove();
 $(".section.oer").remove();
+
+// $("head").append("<link rel='stylesheet' href='webStyle.css'>");
+
+// $(".col_md_6.img").css("padding", "30px");
+$(".col_md_6.img").css("box-sizing", "border-box");
